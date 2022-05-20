128cf655c5f09d652e41dcb4b43b0aa4da8930a7
Andrew Milson <andrew.j.milson@gmail.com>
2022-05-20 23:51:52 +1000

    Re-arrange structure and begin to add comments

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,155 @@
 // #![no_std]
 
-const fn ROL64(a: u64, n: u64) -> u64 {
-    (a >> (64 - (n % 64))) + (a << (n % 64))
+const LANE_DIAM: usize = 5;
+
+type Lanes = [[u64; LANE_DIAM]; LANE_DIAM];
+
+const STATE_WIDTH: usize = 200;
+
+type State = [u8; STATE_WIDTH];
+
+pub const fn SHAKE128<const INPUT_BYTES_LEN: usize, const OUTPUT_BYTES_LEN: usize>(
+    input: [u8; INPUT_BYTES_LEN],
+) -> [u8; OUTPUT_BYTES_LEN] {
+    const RATE: usize = 1344;
+    const CAPACITY: usize = 256;
+    keccak::<RATE, CAPACITY, INPUT_BYTES_LEN, OUTPUT_BYTES_LEN>(input, 0x1F)
 }
 
-const LANE_DIAM: usize = 5;
-type Lanes = [[u64; LANE_DIAM]; LANE_DIAM];
+pub const fn SHAKE256<const INPUT_BYTES_LEN: usize, const OUTPUT_BYTES_LEN: usize>(
+    input: [u8; INPUT_BYTES_LEN],
+) -> [u8; OUTPUT_BYTES_LEN] {
+    const RATE: usize = 1088;
+    const CAPACITY: usize = 512;
+    keccak::<RATE, CAPACITY, INPUT_BYTES_LEN, OUTPUT_BYTES_LEN>(input, 0x1F)
+}
+
+pub const fn SHA3_224<const INPUT_BYTES_LEN: usize>(input: [u8; INPUT_BYTES_LEN]) -> [u8; 28] {
+    const RATE: usize = 1152;
+    const CAPACITY: usize = 448;
+    const OUTPUT_BYTES_LEN: usize = 28;
+    keccak::<RATE, CAPACITY, INPUT_BYTES_LEN, OUTPUT_BYTES_LEN>(input, 0x06)
+}
+
+pub const fn SHA3_256<const INPUT_BYTES_LEN: usize>(input: [u8; INPUT_BYTES_LEN]) -> [u8; 32] {
+    const RATE: usize = 1088;
+    const CAPACITY: usize = 512;
+    const OUTPUT_BYTES_LEN: usize = 32;
+    keccak::<RATE, CAPACITY, INPUT_BYTES_LEN, OUTPUT_BYTES_LEN>(input, 0x06)
+}
+
+pub const fn SHA3_384<const INPUT_BYTES_LEN: usize>(input: [u8; INPUT_BYTES_LEN]) -> [u8; 48] {
+    const RATE: usize = 832;
+    const CAPACITY: usize = 768;
+    const OUTPUT_BYTES_LEN: usize = 48;
+    keccak::<RATE, CAPACITY, INPUT_BYTES_LEN, OUTPUT_BYTES_LEN>(input, 0x06)
+}
+
+pub const fn SHA3_512<const INPUT_BYTES_LEN: usize>(input: [u8; INPUT_BYTES_LEN]) -> [u8; 64] {
+    const RATE: usize = 576;
+    const CAPACITY: usize = 1024;
+    const OUTPUT_BYTES_LEN: usize = 64;
+    keccak::<RATE, CAPACITY, INPUT_BYTES_LEN, OUTPUT_BYTES_LEN>(input, 0x06)
+}
+
+const fn keccak<
+    const RATE: usize,
+    const CAPACITY: usize,
+    const INPUT_BYTES_LEN: usize,
+    const OUTPUT_BYTES_LEN: usize,
+>(
+    input: [u8; INPUT_BYTES_LEN],
+    delimited_suffix: u8,
+) -> [u8; OUTPUT_BYTES_LEN] {
+    if RATE + CAPACITY != 1600 {
+        panic!();
+    }
+
+    if RATE % 8 != 0 {
+        panic!("Rate needs to be a multiple of 8");
+    }
+
+    let mut output = [0; OUTPUT_BYTES_LEN];
+    let mut state = [0; STATE_WIDTH];
+    let rate_in_bytes = RATE / 8;
+    let mut block_size = 0;
+    let mut input_offset = 0;
+    // absorb all the input blocks
+    while input_offset < INPUT_BYTES_LEN {
+        block_size = min(INPUT_BYTES_LEN - input_offset, rate_in_bytes);
+        let mut i = 0;
+        while i < block_size {
+            state[i] ^= input[i + input_offset];
+            i += 1;
+        }
+        input_offset += block_size;
+        if block_size == rate_in_bytes {
+            state = keccak_f1600(state);
+            block_size = 0;
+        }
+    }
+    // do the padding and switch to the squeezing phase
+    state[block_size] ^= delimited_suffix;
+    if delimited_suffix & 0x80 != 0 && block_size == rate_in_bytes - 1 {
+        state = keccak_f1600(state);
+    }
+    state[rate_in_bytes - 1] ^= 0x80;
+    state = keccak_f1600(state);
+    // squeeze out all the output blocks
+    let mut output_offset = 0;
+    while output_offset < OUTPUT_BYTES_LEN {
+        block_size = min(OUTPUT_BYTES_LEN - output_offset, rate_in_bytes);
+        let mut j = 0;
+        while j < block_size {
+            output[j] += state[j];
+            j += 1;
+        }
+        output_offset += block_size;
+        if output_offset < OUTPUT_BYTES_LEN {
+            state = keccak_f1600(state);
+        }
+    }
+    output
+}
+
+const fn keccak_f1600(state: State) -> State {
+    let mut lanes = [[0; LANE_DIAM]; LANE_DIAM];
+    let mut buffer = [0; 8];
+    let mut x = 0;
+    while x < LANE_DIAM {
+        let mut y = 0;
+        while y < LANE_DIAM {
+            let start = 8 * (x + LANE_DIAM * y);
+            let mut z = 0;
+            while z < buffer.len() {
+                buffer[z] = state[start + z];
+                z += 1;
+            }
+            lanes[x][y] = u64::from_le_bytes(buffer);
+            y += 1;
+        }
+        x += 1;
+    }
+    let lanes = keccak_f1600_on_lanes(lanes);
+    let mut state = [0; STATE_WIDTH];
+    let mut x = 0;
+    while x < LANE_DIAM {
+        let mut y = 0;
+        while y < LANE_DIAM {
+            let buffer = lanes[x][y].to_le_bytes();
+            let start = 8 * (x + LANE_DIAM * y);
+            let mut z = 0;
+            while z < buffer.len() {
+                state[start + z] = buffer[z];
+                z += 1;
+            }
+            y += 1;
+        }
+        x += 1;
+    }
+    state
+}
+
 const fn keccak_f1600_on_lanes(lanes: Lanes) -> Lanes {
     let mut lanes = lanes;
     let mut R = 1u32;
@@ -84,136 +228,12 @@ const fn keccak_f1600_on_lanes(lanes: Lanes) -> Lanes {
     lanes
 }
 
-const STATE_WIDTH: usize = 200;
-type State = [u8; STATE_WIDTH];
-const fn keccak_f1600(state: State) -> State {
-    let mut lanes = [[0; LANE_DIAM]; LANE_DIAM];
-    let mut buffer = [0; 8];
-    let mut x = 0;
-    while x < LANE_DIAM {
-        let mut y = 0;
-        while y < LANE_DIAM {
-            let start = 8 * (x + LANE_DIAM * y);
-            let mut z = 0;
-            while z < buffer.len() {
-                buffer[z] = state[start + z];
-                z += 1;
-            }
-            lanes[x][y] = u64::from_le_bytes(buffer);
-            y += 1;
-        }
-        x += 1;
-    }
-    let lanes = keccak_f1600_on_lanes(lanes);
-    let mut state = [0; STATE_WIDTH];
-    let mut x = 0;
-    while x < LANE_DIAM {
-        let mut y = 0;
-        while y < LANE_DIAM {
-            let buffer = lanes[x][y].to_le_bytes();
-            let start = 8 * (x + LANE_DIAM * y);
-            let mut z = 0;
-            while z < buffer.len() {
-                state[start + z] = buffer[z];
-                z += 1;
-            }
-            y += 1;
-        }
-        x += 1;
-    }
-    state
+const fn ROL64(a: u64, n: u64) -> u64 {
+    (a >> (64 - (n % 64))) + (a << (n % 64))
 }
 
 const fn min(a: usize, b: usize) -> usize {
     [a, b][(a > b) as usize]
-}
-
-const fn keccak<
-    const RATE: usize,
-    const CAPACITY: usize,
-    const INPUT_LEN: usize,
-    const OUTPUT_LEN: usize,
->(
-    input: [u8; INPUT_LEN],
-    delimited_suffix: u8,
-) -> [u8; OUTPUT_LEN] {
-    if RATE + CAPACITY != 1600 {
-        panic!();
-    }
-
-    if RATE % 8 != 0 {
-        panic!("Rate needs to be a multiple of 8");
-    }
-
-    let mut output = [0; OUTPUT_LEN];
-    let mut state = [0; STATE_WIDTH];
-    let rate_in_bytes = RATE / 8;
-    let mut block_size = 0;
-    let mut input_offset = 0;
-    // absorb all the input blocks
-    while input_offset < INPUT_LEN {
-        block_size = min(INPUT_LEN - input_offset, rate_in_bytes);
-        let mut i = 0;
-        while i < block_size {
-            state[i] ^= input[i + input_offset];
-            i += 1;
-        }
-        input_offset += block_size;
-        if block_size == rate_in_bytes {
-            state = keccak_f1600(state);
-            block_size = 0;
-        }
-    }
-    // do the padding and switch to the squeezing phase
-    state[block_size] ^= delimited_suffix;
-    if delimited_suffix & 0x80 != 0 && block_size == rate_in_bytes - 1 {
-        state = keccak_f1600(state);
-    }
-    state[rate_in_bytes - 1] ^= 0x80;
-    state = keccak_f1600(state);
-    // squeeze out all the output blocks
-    let mut output_offset = 0;
-    while output_offset < OUTPUT_LEN {
-        block_size = min(OUTPUT_LEN - output_offset, rate_in_bytes);
-        let mut j = 0;
-        while j < block_size {
-            output[j] += state[j];
-            j += 1;
-        }
-        output_offset += block_size;
-        if output_offset < OUTPUT_LEN {
-            state = keccak_f1600(state);
-        }
-    }
-    output
-}
-
-pub const fn SHAKE128<const INPUT_LEN: usize, const OUTPUT_LEN: usize>(
-    input: [u8; INPUT_LEN],
-) -> [u8; OUTPUT_LEN] {
-    keccak::<1344, 256, INPUT_LEN, OUTPUT_LEN>(input, 0x1F)
-}
-
-pub const fn SHAKE256<const INPUT_LEN: usize, const OUTPUT_LEN: usize>(
-    input: [u8; INPUT_LEN],
-) -> [u8; OUTPUT_LEN] {
-    keccak::<1088, 512, INPUT_LEN, OUTPUT_LEN>(input, 0x1F)
-}
-
-pub const fn SHA3_224<const INPUT_LEN: usize>(input: [u8; INPUT_LEN]) -> [u8; 28] {
-    keccak::<1152, 448, INPUT_LEN, 28>(input, 0x06)
-}
-
-pub const fn SHA3_256<const INPUT_LEN: usize>(input: [u8; INPUT_LEN]) -> [u8; 32] {
-    keccak::<1088, 512, INPUT_LEN, 32>(input, 0x06)
-}
-
-pub const fn SHA3_384<const INPUT_LEN: usize>(input: [u8; INPUT_LEN]) -> [u8; 48] {
-    keccak::<832, 768, INPUT_LEN, 48>(input, 0x06)
-}
-
-pub const fn SHA3_512<const INPUT_LEN: usize>(input: [u8; INPUT_LEN]) -> [u8; 64] {
-    keccak::<576, 1024, INPUT_LEN, 64>(input, 0x06)
 }
 
 #[cfg(test)]
